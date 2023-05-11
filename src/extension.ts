729470e78e730bd69e953bbcff7a94c9044daf11
duong-vo <vodh2@miamioh.edu>
2023-05-11 04:28:27 -0400

    add panel, got started with the whiteboard

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,34 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sidenotes" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "sidenotes" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sidenotes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sidenotes!');
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand('sidenotes.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from sidenotes!');
+        
+    });
 
-	context.subscriptions.push(disposable);
+    const testPanel = vscode.commands.registerCommand('sidenotes.testPanel', testPanelCallback);
+    
+    
+    context.subscriptions.push(disposable);
 }
 
+function testPanelCallback(): void {
+    const panel = vscode.window.createWebviewPanel(
+        'testPanel',
+        'Test Panel',
+        vscode.ViewColumn.One,
+        {}
+      );
+    
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
